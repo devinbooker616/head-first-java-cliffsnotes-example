2a52b614f84c6018e7757eac35aff2b5943236e0
Devin Booker <dbooker@basecampcodingacademy.org>
2020-01-10 15:13:11 -0600

    Finished app

--- chapter-5-extra-strength-methods/App/src/app/App.java
+++ chapter-5-extra-strength-methods/App/src/app/App.java
@@ -2,6 +2,17 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.err.println(i);
+            }
+
+        }
     }
 }
