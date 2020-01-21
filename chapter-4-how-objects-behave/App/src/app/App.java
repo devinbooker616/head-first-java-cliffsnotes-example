060d56498edfc5f1d7acb58a814f70efcd7defea
Devin Booker <dbooker@basecampcodingacademy.org>
2020-01-21 13:49:52 -0600

    added array to chapter 4

--- chapter-4-how-objects-behave/App/src/app/App.java
+++ chapter-4-how-objects-behave/App/src/app/App.java
@@ -2,6 +2,7 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println(nums);
     }
 }
