36ad58c5f99727e9fbbfa44ca88bf768ca4271c4
Devin Booker <dbooker@basecampcodingacademy.org>
2020-01-29 09:18:01 -0600

    added casting in app

--- chapter-5-extra-strength-methods/App/src/app/App.java
+++ chapter-5-extra-strength-methods/App/src/app/App.java
@@ -17,6 +17,19 @@ public class App {
         int randomNum = (int) (Math.random() * 5);
         System.out.println(randomNum);
 
+        // This is casting
+        long y = 42;
+        int x = (int) y;
+        System.out.println(x);
+
+        long z = 40002;
+        short i = (short) z;
+        System.out.println(i);
+
+        float f = 3.14f;
+        int h = (int) f;
+        // This is the end of the casting
+        System.out.println(h);
         String[] list = { "Something ", "about ", "a ", "foreach ", "loop " };
         for (String words : list) {
             System.out.print(words);
